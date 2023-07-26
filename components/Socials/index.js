5e7287e49e4aa92381e687de0c630f167fadc35c
SinyorDes <sinyordes@protonmail.com>
2023-07-26 01:08:24 +0000

    update dc

--- components/Socials/index.js
+++ components/Socials/index.js
@@ -7,16 +7,16 @@ export default function Socials() {
                 <span>Coming Soon</span>
             </div>
             <a
-                onClick={
-                    (e) => {
-                        e.preventDefault();
-                        document.querySelector('.coming-soon').classList.add('activeModal');
-                        setTimeout(() => {
-                            document.querySelector('.coming-soon').classList.remove('activeModal');
-                        }, 1000);
-                    }
-                }
-                href="#">
+             //   onClick={
+             //       (e) => {
+             //           e.preventDefault();
+             //           document.querySelector('.coming-soon').classList.add('activeModal');
+             //           setTimeout(() => {
+             //               document.querySelector('.coming-soon').classList.remove('activeModal');
+             //           }, 1000);
+             //       }
+             //   }
+                href="https://discord.gg/2WwcS86J">
                 <img src="/images/discord-square-color-icon.png" alt="discord" />
             </a>
             <a href="https://twitter.com/StarknetBunny" target="_blank">
