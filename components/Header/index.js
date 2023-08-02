b1135ff28ee539c05d85c4982c27a9a0d236bc41
SinyorDes <sinyordesyt@gmail.com>
2023-08-02 22:22:14 +0300

    Update index.js

--- components/Header/index.js
+++ components/Header/index.js
@@ -59,11 +59,7 @@ export default function Header({theme = "light"}) {
                         <li><a href="/">Home</a></li>
                         <li><a href="/bunny-land">BunnyLand</a></li>
                         <li><a href="/roadmap">Roadmap</a></li>
-                        <li className="goldenNav"><a href="/golden-carrot">Golden Carrot</a></li>
-
-                  
-                      
-                        
+                        {/**<li className="goldenNav"><a href="/golden-carrot">Golden Carrot</a></li> */}
                     </ul>
                 </nav>
             </div>
@@ -83,8 +79,7 @@ export default function Header({theme = "light"}) {
                         <li><a href="/">Home</a></li>
                         <li><a href="/bunny-land">BunnyLand</a></li>
                         <li><a href="/roadmap">Roadmap</a></li>
-                        <li className="goldenNav"><a href="/golden-carrot">Golden Carrot</a></li>
-                        
+                        {/*<li className="goldenNav"><a href="/golden-carrot">Golden Carrot</a></li>*/}                        
                     </ul>
                     <img src="/images/bunny-left.png" alt="bunny"/>
                 </nav>
